9ba3fe0a40a7067b4e2753a829624879165c6b0c
snehal-iblinfotech <snehal.chaudhari@iblinfotech.com>
2021-10-08 13:32:58 +0530

    errot handling

--- controllers/post.js
+++ controllers/post.js
@@ -2,9 +2,13 @@
 import Post from "../models/blog.js";
 
 export const getAllPosts = async (req, res) => {
-  const posts = await Post.find();
-  console.log(posts);
-  res.status(200).json(posts);
+  try {
+    const posts = await Post.find();
+    console.log(posts);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 export const createPost = async (req, res) => {
   console.log(req.body);
@@ -13,7 +17,7 @@ export const createPost = async (req, res) => {
     await post.save();
     res.status(200).json(post);
   } catch (error) {
-    console.log(error.message);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
@@ -22,18 +26,48 @@ export const deletePost = async (req, res) => {
     await Post.findByIdAndRemove(req.params.id);
     res.status(200).json({ msg: "post deleted successfully" });
   } catch (error) {
-    console.log(error.message);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
 export const editPost = async (req, res) => {
   const id = req.params.id;
   const post = req.body;
-  console.log(id, post);
   try {
     const updatedPost = await Post.findByIdAndUpdate(id, post, { new: true });
     res.status(200).json(updatedPost);
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
+};
+
+export const likePost = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const post = await Post.findById(id);
+    if (
+      post.likes.filter((like) => like.user.toString() === req.userId).length >
+      0
+    ) {
+      return res.status(404).jsonp({ msg: "Post already like" });
+    }
+
+    post.likes.unshift({ user: req.userId });
+    await post.save();
+  } catch (error) {
+    console.log(error);
+    if (error.kind === "ObjectId") {
+      return res.status(404).jsonp({ msg: "No post found" });
+    }
+    res.status(500).send("Server Error");
+  }
+};
+
+export const unlikePost = async(req, res);
+
+export const commentPost = (req, res) => {
+  try {
+  } catch (error) {}
 };
